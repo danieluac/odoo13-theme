c8d3896643a4206b8518e272e582c59126723b24
António Daniel <antoniocordeiro228@gmail.com>
2020-11-09 22:08:14 +0100

    diferntes cores em cada menu personalizado da página
    Took 49 minutes

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+// var url = window.location.pathname;
+// var element2 = document.querySelectorAll('.theme_danger a[href="'+url+'"]')[0];
+// console.log(element2)
+//  element.classList.remove("bg-dark");
+//  element2.classList.add("theme_danger");
+
+
+})(jQuery); // End of use strict
