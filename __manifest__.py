f2c01ec5aa417401bfcb85c385130ef51c50a37c
António Daniel <antoniocordeiro228@gmail.com>
2020-11-09 14:30:41 +0100

    tema personalizado, cada página com sua cor
    Took 8 hours 0 minutes

--- __manifest__.py
+++ __manifest__.py
@@ -10,23 +10,25 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Daniel AC",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Theme/Loengo',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+
+        'views/assets.xml',
+        'views/layout.xml',
         'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
