8a963ab45c362825db92910cff02d9a1c17b3ea1
António Daniel <antoniocordeiro228@gmail.com>
2020-11-12 08:54:28 +0100

    varias flohas de estilos para os diferentes theme
    Took 11 seconds

--- __manifest__.py
+++ __manifest__.py
@@ -2,20 +2,15 @@
 {
     'name': "theme_loengo",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """ Loengo theme with three color,
+        """,
 
     'description': """
-        Long description of module's purpose
+        loengo theme
     """,
 
     'author': "Daniel AC",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'website': "http://www.facebook.com/danieluacDev",
     'category': 'Theme/Loengo',
     'version': '0.1',
 
